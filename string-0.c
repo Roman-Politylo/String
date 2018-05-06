206aa762bf289084a87b14ea3a0d86f5d92bd7c0
Roman <roman.politylo777@gmail.com>
2018-05-06 10:00:06 -0400

    New commit

---
+++ string-0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+char s[100];
+
+    scanf("%s", s);
+    for (int i = 0; i < strlen(s); i++)
+    printf("%c\n", s[i]);
+}
